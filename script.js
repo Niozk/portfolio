70cd7a4f75ae67e53d2085742da30b0ecbf52749
Niozk <99679240+Niozk@users.noreply.github.com>
2023-05-31 00:56:05 +0200

    Intro klaar
    -Naam animatie ge-add
    -Parallex scrolling bij waves ge-add

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 import { animate } from "https://cdn.jsdelivr.net/npm/motion@10.16.2/+esm";
+import simpleParallaxJs from 'https://cdn.jsdelivr.net/npm/simple-parallax-js@5.6.2/+esm'
 
 animate("#arrow-down",
     { y: [0, -10, 10, 0] },
@@ -9,4 +10,24 @@ animate("#arrow-down",
     }
 );
 
-console.log('hoiiii')
+var image = document.getElementsByClassName('waves');
+new simpleParallaxJs(image, {
+	scale: 2
+});
+
+const enhance = id => {
+    const element = document.getElementById(id),
+        text = element.innerText.split("");
+    
+    element.innerText = "";
+
+    text.forEach(letter => {
+        const span = document.createElement("span");
+        span.className = "letter";
+        span.innerText = letter;
+        element.appendChild(span);
+    });
+}
+
+enhance('fname');
+enhance('lname');
