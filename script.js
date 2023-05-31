7a99eda2bd093ebffcc0e8fa0fc03bbd70060674
Niozk <99679240+Niozk@users.noreply.github.com>
2023-05-31 14:26:01 +0200

    About me ge-add
    -Responsive
    -Geen animaties nog
    -Grote bugfixes

--- script.js
+++ script.js
@@ -1,20 +1,6 @@
 import { animate } from "https://cdn.jsdelivr.net/npm/motion@10.16.2/+esm";
 import simpleParallaxJs from 'https://cdn.jsdelivr.net/npm/simple-parallax-js@5.6.2/+esm'
 
-animate("#arrow-down",
-    { y: [0, -10, 10, 0] },
-    {
-        duration: 2,
-        offset: [0, 0.25, 0.75],
-        repeat: Infinity
-    }
-);
-
-var image = document.getElementsByClassName('waves');
-new simpleParallaxJs(image, {
-	scale: 2
-});
-
 const enhance = id => {
     const element = document.getElementById(id),
         text = element.innerText.split("");
@@ -28,6 +14,19 @@ const enhance = id => {
         element.appendChild(span);
     });
 }
-
 enhance('fname');
 enhance('lname');
+
+animate("#arrow-down",
+    { y: [0, -10, 10, 0] },
+    {
+        duration: 2,
+        offset: [0, 0.25, 0.75],
+        repeat: Infinity
+    }
+);
+
+let image = document.getElementsByClassName('waves');
+new simpleParallaxJs(image, {
+	scale: 2
+});
