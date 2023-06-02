c3c28d2c5dc865d7dd312e21a7c3d2d13fc616c0
Niozk <99679240+Niozk@users.noreply.github.com>
2023-06-02 16:13:34 +0200

    Release 1.0
    -Animaties ge-add

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-import { animate } from "https://cdn.jsdelivr.net/npm/motion@10.16.2/+esm";
+import { animate, inView } from "https://cdn.jsdelivr.net/npm/motion@10.16.2/+esm";
 import simpleParallaxJs from 'https://cdn.jsdelivr.net/npm/simple-parallax-js@5.6.2/+esm'
-
+;
 const enhance = id => {
     const element = document.getElementById(id),
         text = element.innerText.split("");
@@ -13,9 +13,31 @@ const enhance = id => {
         span.innerText = letter;
         element.appendChild(span);
     });
-}
+};
 enhance('fname');
 enhance('lname');
+
+let breakpoint = window.matchMedia("(min-width: 860px)");
+function animationBreakpoint(breakpoint) {
+  if (breakpoint.matches) {
+    animate(
+        "header h1",
+        { x: [-100, 0], opacity: [0, 1] },
+        {
+          duration: 1.4
+        }
+    );
+  } else {
+    animate(
+        "header h1",
+        { y: [50, 0], opacity: [0, 1] },
+        {
+            duration: 1.4
+        }
+    );
+  }
+}
+animationBreakpoint(breakpoint);
 
 animate("#arrow-down",
     { y: [0, -10, 10, 0] },
@@ -30,3 +52,32 @@ let image = document.getElementsByClassName('waves');
 new simpleParallaxJs(image, {
 	scale: 2
 });
+
+inView(".about-me-container", () => {
+    animate(
+        ".about-me-container",
+        { y: [50, 0], opacity: [0, 1] },
+        { delay: 0.5, duration: 1.4 }
+    );
+});
+
+//----------------------------------------------------------------
+
+const allProjects = document.getElementById('projects-container').children;
+let projectArray = [];
+
+for (let i = 0; i < allProjects.length; i++) {
+  const project = allProjects[i];
+
+  projectArray.push(project);
+}
+
+for (let i = 0; i < projectArray.length; i++) {
+    inView(projectArray[i], () => {
+        animate(
+            projectArray[i],
+            { y: [50, 0], opacity: [0, 1] },
+            { delay: 0.5, duration: 1.4 }
+        );
+    });
+}
